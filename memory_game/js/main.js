fbfa3ede740e106d48fa44c8592919838a626599
gemInstallButlet <euruzilys@gmail.com>
2019-08-06 19:52:33 +0700

    Created game logic (Control Flow , JS)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-let cardOne = 'queen';
-let cardTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardFour);
+console.log('User flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!');
+  }
+  else {
+    alert('Sorry, try again.');
+  }
+}
